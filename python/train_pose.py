54326b0baf26215919f3426162a743f4e213cf0b
huhu233 <925393268@qq.com>
2026-06-09 15:23:09 +0800

    修改训练脚本

--- python/train_pose.py
+++ python/train_pose.py
@@ -7,40 +7,45 @@ import torch
 
 def train():
     # 数据集配置文件路径（生成脚本输出的 dataset.yaml）
-    dataset_yaml = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "generated_dataset", "dataset_v2", "dataset.yaml")
+    dataset_yaml = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "generated_dataset", "dataset_v3", "dataset.yaml")
     if not os.path.exists(dataset_yaml):
         raise FileNotFoundError(f"Dataset yaml not found: {dataset_yaml}")
 
-    model = YOLO("yolo26n-pose.yaml")
+    model = YOLO("yolo11n-pose.yaml")
 
     # 训练参数（关键：禁用水平/垂直翻转）
     results = model.train(
         data=dataset_yaml,
-        epochs=30,
+        epochs=100,
         imgsz=640,
-        batch=16,
+        batch=64,
         device=0 if torch.cuda.is_available() else "cpu",                # GPU ID，若用 CPU 设为 'cpu'
         workers=8,
         patience=50,
         save=True,
         project="power_rune_train",
-        name="power_rune_exp2",
-        optimizer="AdamW",
+        name="power_rune_exp3",
+        # optimizer="AdamW",
         # --------------------------------------------------
         # 禁用镜像翻转增强
-        fliplr=0.0,               # 水平翻转概率 0%
-        flipud=0.0,               # 垂直翻转概率 0%
-        # # 其他增强可根据需要调整，但不使用翻转
-        # degrees=0.0,             # 旋转角度（若需要可保留，但注意关键点对称）
-        # translate=0.1,           # 平移增强
-        # scale=0.5,               # 缩放增强
-        # shear=0.0,               # 剪切变换
-        # perspective=0.0,         # 透视变换
-        # # --------------------------------------------------
-        # # 注意：YOLOv8 还支持 mosaic=1.0, mixup=0.0 等，这些不影响镜像翻转，可保留
-        # mosaic=1.0,
-        # mixup=0.0,
-        # copy_paste=0.0,
+        hsv_h= 0.1,
+        hsv_s= 0.8,
+        hsv_v= 0.5,
+        degrees= 10.0,
+        translate= 0.1,
+        scale= 0.3,
+        shear= 3.0,
+        perspective= 0.0003,
+        flipud= 0.0,
+        fliplr= 0.0,
+        bgr= 0.0,
+        mosaic= 0.5,
+        mixup= 0.0,
+        cutmix= 0.0,
+        copy_paste= 0.0,
+        erasing= 0.5,
+        # --------------------------------------------------
+        close_mosaic = 30,
     )
 
     print("Training finished.")
